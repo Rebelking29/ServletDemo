2a5b7341a7297c8aab569c4b77df390cc49a578e
virus <harshumd29301@gmail.com>
2024-05-14 18:51:25 +0530

    Insert login implemented in GetData Servlet Class.

--- ServletDemo/src/main/java/com/servlet/GetData.java
+++ ServletDemo/src/main/java/com/servlet/GetData.java
@@ -1,7 +1,11 @@
 package com.servlet;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
+import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -14,6 +18,33 @@ public class GetData extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
+		ServletConfig config = getServletConfig();
+		
+		String driver = config.getInitParameter("driver");
+		String url = config.getInitParameter("url");
+		String user = config.getInitParameter("user");
+		String pass = config.getInitParameter("pass");
+		
+		int id = Integer.parseInt(req.getParameter("id"));
+		String name = req.getParameter("name");
+		String des = req.getParameter("des");
+		
+		try {
+			
+			Class.forName(driver);
+			
+			Connection con = DriverManager.getConnection(url, user, pass);
+			PreparedStatement ps = con.prepareStatement("insert into employees value(?,?,?)");
+			ps.setInt(1, id);
+			ps.setString(2, name);
+			ps.setString(3, des);
+			
+			int i = ps.executeUpdate();
+			boolean s=(i>0)?true:false;
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
